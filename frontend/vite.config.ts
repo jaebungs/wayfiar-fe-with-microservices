cc23613b4de708b180747d0c3c972d22fc45058f
jaebungs <jaebungs@gmail.com>
2025-06-08 17:30:05 -0400

    add svgr for simple use of icons within the repo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    svgr()
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+    },
+  },
 })
