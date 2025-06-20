06a89d073d6a3ebfa7e0e3397ecd03f53c76d631
jaebungs <jaebungs@gmail.com>
2025-06-19 22:09:29 -0400

    style sign up page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,12 +1,33 @@
-import EmailInput from '@/components/common/EmailInput';
+import { Link } from 'react-router-dom'
+import EmailInput from '@/components/common/EmailInput'
+import WayfairTextLogo from '@/assets/icons/wayfairTextLogo.svg?react'
 
 const Signin = () => {
   return (
-    <div>
-        <EmailInput 
-            placeholder="Enter your email"
-            required
-        />
+    <div className="flex flex-col justify-center items-center">
+        <div className="w-[90%] mx-auto py-8 flex justify-between border-b border-gray-100 items-center">
+            <Link to="/" className="w-[210px] h-[45px] block">
+              <WayfairTextLogo className="w-full h-full" />
+            </Link>
+            <p className='font-medium text-base'>Secure Login</p>
+        </div>
+        
+        <div className="flex flex-col justify-center items-center gap-1 max-w-[400px] mt-10">
+            <h1 className='text-[1.5625rem] font-bold text-center'>Enter your email address to sign in or to create an account</h1>
+            <h2 className='text-[1.5625rem] text-center'>They don't have Oauth login.</h2>
+            <div className='flex flex-col justify-center items-center w-full gap-4'>
+                <EmailInput
+                    placeholder="Email Address"
+                    className='w-full'
+                    required
+                />
+                <button className='w-full p-4 rounded-4xl text-white font-medium bg-purple-100 hover:bg-purple-200'>
+                    Continue
+                </button>
+            </div>
+
+        </div>
+
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         white: 'var(--white)',
         'dark-100': 'var(--dark-100)',
+        'gray-100': 'var(--color-gray-100)',
         'purple-100': 'var(--color-purple-100)',
         'purple-200': 'var(--color-purple-200)',
       },
