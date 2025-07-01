2611865167d7d897bae3ceee297b89c71f0ff868
jaebungs <jaebungs@gmail.com>
2025-07-01 11:53:32 -0400

    implement hash functions in sign up and sign in

--- services/auth-service/src/db/userModel.ts
+++ services/auth-service/src/db/userModel.ts
@@ -1,4 +1,5 @@
 import pool from './connection'
+import PasswordHash from '@/services/passwordHash'
 
 interface CreateUserData {
     email: string
@@ -22,22 +23,27 @@ export const findUserByEmail = async (email: string): Promise<User | null> => {
 
 export async function createUser(userData: CreateUserData): Promise<User> {
     const { email, password, role } = userData
-    
+    const hashedPassword = await PasswordHash.hashPassword(password)
     const query = `
         INSERT INTO users (email, password, role)
         VALUES ($1, $2, $3)
         RETURNING id, email, role, created_at, updated_at
     `
-    const result = await pool.query(query, [email, password, role])
+
+    const result = await pool.query(query, [email, hashedPassword, role])
+
     return result.rows[0]
 }
 
 export async function signInUser(email: string, password: string): Promise<User | null> {
-    const query = `
-        SELECT id, email, role, created_at, updated_at 
-        FROM users 
-        WHERE email = $1 AND password = $2
-    `
-    const result = await pool.query(query, [email, password])
-    return result.rows[0] || null
+    const query = `SELECT * FROM users WHERE email = $1`
+    const result = await pool.query(query, [email])
+    const user = result.rows[0]
+
+    if (!user) return null
+
+    const isPasswordMatch = await PasswordHash.comparePassword(password, user.password)
+    if (!isPasswordMatch) return null
+    
+    return { id: user.id, email: user.email, role: user.role, created_at: user.created_at, updated_at: user.updated_at}
 }
