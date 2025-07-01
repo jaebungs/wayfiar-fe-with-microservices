c84a3676ad9292bf174d4eca893606b8ca7c4391
jaebungs <jaebungs@gmail.com>
2025-06-30 20:14:17 -0400

    add user sign in route

--- services/auth-service/src/routes/userRoutes.ts
+++ services/auth-service/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { signUpUser, checkExistEmail } from '@/controllers/userController'
+import { signUpUser, signInUser, checkExistEmail } from '@/controllers/userController'
 
 const router = Router()
 
 // auth/signup
 router.post('/signup', signUpUser)
+router.post('/signin', signInUser)
 router.get('/email', checkExistEmail)
 
 export default router
